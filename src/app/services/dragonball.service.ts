9e005f7bf3e790e137715763e06c381afd3a6a65
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-08-20 10:56:09 -0500

    feat: implement character loading from local storage in DragonballService

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@angular/core';
+import { effect, Injectable } from '@angular/core';
 
 import { signal } from '@angular/core';
 import { Character } from '../interfaces/caracter.interface';
 
+// Es creada por fuera porque no tiene que ver con el servicio sino que se usa una única vez
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+
+  // No es la mejor práctica (Hay que validar):
+  return characters ? JSON.parse(characters) : [];
+};
+
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+  saveToLocalStorage = effect(() => {
+    console.log(`Character count ${this.characters().length}`);
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(newCharacter: Character) {
     this.characters.update((list) => [...list, newCharacter]);
