7370852f929256960b63417d5f6d1cabd01bc149
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-08-19 23:55:51 -0500

    feat: add DragonBall page component with HTML template and TypeScript logic

---
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -0,0 +1,46 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [],
+  templateUrl: './dragonball-page.html',
+  styleUrl: './dragonball-page.css',
+})
+export class DragonballPage {
+  name = signal('');
+  power = signal(0);
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    // { id: 2, name: 'Vegeta', power: 8000 },
+    // { id: 3, name: 'Piccolo', power: 3000 },
+    // { id: 4, name: 'Yamcha', power: 500 },
+  ]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+}
