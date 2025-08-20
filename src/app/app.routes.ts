9c5bddf1d5612c25b42884663bd59d00df7847e3
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-08-19 23:54:38 -0500

    feat: Project Initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPage } from './pages/dragonball/dragonball-page';
+import { DragonballSuperPage } from './pages/dragonball-super/dragonball-super-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CounterPageComponent,
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent,
+  },
+  {
+    path: 'dragonball',
+    component: DragonballPage,
+  },
+  {
+    path: 'dragonball-super',
+    component: DragonballSuperPage,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
