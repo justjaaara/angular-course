9751324195d131b3b27da39b2ea87f18ce38cd83
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-08-20 11:05:55 -0500

    feat: add HashLocationStrategy to application configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,18 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+
+    // HashStrategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
   ],
 };
